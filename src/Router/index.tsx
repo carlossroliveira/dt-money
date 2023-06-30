5a95bc751a3170f09cb07a6677dcca255b50380b
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-06-29 21:16:47 -0300

    Criação de componente e estilos

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,16 +1,15 @@
 // Packages
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// Components
-import { Layout } from '../components/Layout'
+// Page
+import { Transactions } from '../pages/Transactions'
 
 export const DefaultRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Transactions />}>
           <Route path="/" element={<h1>Paga 1</h1>} />
-          <Route path="/page" element={<h1>Paga 2</h1>} />
         </Route>
 
         <Route path="*" element={<h1>Error</h1>} />
