54a8efe27f080da7cc9444e4187c053ad372dcd2
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-06-29 21:16:20 -0300

    Ajustes e importações de imagens

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -15,13 +15,13 @@ export const GlobalStyle = createGlobalStyle`
  box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
 }
 
-body, input, texarea, button {
+body, input, textarea, button {
   font-size: 1rem;
   font-weight: 400;
   font-family: 'Roboto', sans-serif;
 
-  color: ${({ theme }) => theme['gray-300']};
-  background-color: ${({ theme }) => theme['gray-900']};
+  color: ${({ theme }) => theme['gray-100']};
+  background-color: ${({ theme }) => theme['gray-800']};
 }
 
 ::-webkit-scrollbar {
