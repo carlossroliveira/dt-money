bc70bcb7cbeaf822d4622f016132a566e84193fa
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-06-29 22:15:55 -0300

    Criação de novo componente NewTransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
 
+import { NewTransactionModal } from '../NewTransactionModal'
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
 export function Header() {
@@ -15,15 +17,7 @@ export function Header() {
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
