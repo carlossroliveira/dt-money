d76ea6da85e71fcaa055d980a804ca1c3c50d075
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-07-01 22:32:14 -0300

    alteração na rota

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -8,9 +8,7 @@ export const DefaultRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Transactions />}>
-          <Route path="/" element={<h1>Paga 1</h1>} />
-        </Route>
+        <Route path="/" element={<Transactions />} />
 
         <Route path="*" element={<h1>Error</h1>} />
       </Routes>
